bb182c0112ef37b424eeccfd9f2778503c6ee0e2
bpolly <Braden@dhcp-164-107-111-198.osuwireless.ohio-state.edu>
2014-12-08 16:20:45 -0500

    Added teach and original HelloWorld

---
+++ Teach/HelloWorld.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello, World");
+		String butts = "butts go here. Boobs go here as well";
+		System.out.println(butts);
+		int bootays = 4;
+		System.out.println("Number of bootays: " + bootays + " bootays");
+		bootays = bootays + 3;
+		System.out.println("Number of bootays: " + bootays + " bootays");
+		
+		// prints out bootays 
+		while(bootays > 0){
+			System.out.println("Bootays left: " + bootays);
+			bootays--;
+		}
+		while(bootays < 10){
+			System.out.println("Bootays left: " + bootays);
+			bootays++;
+		}
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Input a number: ");
+		String hiney="";
+		try {
+			hiney = input.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Hiney: " + hiney);
+		int hiney2 = Integer.parseInt(hiney);
+		
+		if()
+		
+	}
+
+	
+}
